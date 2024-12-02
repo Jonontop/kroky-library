248eee681ebc0d114e09ffa75458b7c37260ef6b
NinjaCof <jon.pecar@gmail.com>
2024-12-02 14:14:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,14 +9,15 @@ class Kroky:
         self.main_url = "https://www.kroky.si/2016/"
         self.menu = []
         self.session = requests.Session()
-        self.response = self.session.post(self.main_url, data={"username": self.username, "password": self.password}, params={"mod": "register", "action": "login"})
+        self.response = self.session.post(self.main_url, data={"username": self.username, "password": self.password},
+                                          params={"mod": "register", "action": "login"})
+        soup = bs4.BeautifulSoup(self.response.text, "html.parser")
+        self.response_status = False if soup.find('font', color="Red") else True
 
-    def get_menu(self, pos):
-        day = ["pon", "tor", "sre", "cet", "pet", "sob"]
+    def get_menu(self, pos, day=("pon", "tor", "sre", "cet", "pet", "sob")):
         menu = []
 
-
-        if self.response.ok:
+        if self.response_status:
             print("Login successful")
 
             # Access the main URL using the same session
@@ -25,8 +26,7 @@ class Kroky:
             if main_response.ok:
                 soup = bs4.BeautifulSoup(main_response.text, "html.parser")
                 for i in day:
-                    day_menu = {}
-                    day_menu[i] = []
+                    day_menu = {i: []}
                     for k in range(1, 12):
                         for j in soup.find_all("td", class_=f"st_menija_{k}_{i}"):
                             day_menu[i].append({
@@ -36,11 +36,11 @@ class Kroky:
                     menu.append(day_menu)
 
                 self.menu = menu
-                return menu
+                return menu[0]
             else:
                 return f"Failed to access main URL: {main_response.status_code}"
         else:
-            return f"Login failed: {self.response.status_code}"
+            return (f"Login failed: napacni vhodni podatki")
 
     def select_meal(self, date, id):
 
@@ -50,11 +50,36 @@ class Kroky:
         }
 
         selection_response = self.session.post(self.main_url, data=selection_data,
-                                          headers={'Content-Type': 'application/x-www-form-urlencoded'},
-                                          params={"mod": "register", "action": "user2date2menu"})
+                                               headers={'Content-Type': 'application/x-www-form-urlencoded'},
+                                               params={"mod": "register", "action": "user2date2menu"})
 
         if not selection_response.ok:
             return f"Failed to select meal with status code: {selection_response.status_code}", 500
 
         return "Meal selected successfully!"
 
+
+    def user_info(self):
+        
+
+
+    def change_password(self, password: str):
+        if self.response.ok:
+            main_response = self.session.get(self.main_url, params={"mod": "register", "action": "editProfile"})
+            if main_response.ok:
+                selection_data = {
+                    "password": password,
+                    "password2": password,
+                }
+
+        selection_response = self.session.post(self.main_url, data=selection_data,
+                                               headers={'Content-Type': 'application/x-www-form-urlencoded'},
+                                               params={"mod": "register", "action": "editProfile"})
+
+        if selection_response.ok:
+            return "dela"
+        return "ne dela!"
+
+
+kroky = Kroky("pe-jon", "q460jk")
+print(kroky.get_menu(0))
